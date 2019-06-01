84d4c5615e8c7e64ee1678a6abb549d15c63b559
Alex Gain <doctorjuice1@gmail.com>
2019-06-01 00:06:48 -0400

    saving image

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 python3 inference_visualization_ani.py --interv=85 --smooth=40
 gdrive update 1S17ImWj0h9AwDd7ZYq2inFpE8xrBwTTT accelerometer.gif
+gdrive update 1A0dFexMsBZWoY5xnKj5q70pBgblRyRkq last_input.png
